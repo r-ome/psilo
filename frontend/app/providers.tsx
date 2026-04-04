2379e73c2945780da329b99a75918a5343889cac
Jerome Agapay <jerome.arceo.agapay@gmail.com>
2026-04-04 14:28:14 +0800

    chore: remove unused next-auth scaffolding

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 import { AuthProvider } from "./context/AuthContext";
 import { TooltipProvider } from "./components/ui/tooltip";
@@ -10,13 +9,11 @@ import { ThemeProvider } from "./components/theme-provider";
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      <SessionProvider>
-        <AuthProvider>
-          <TooltipProvider>
-            <UploadProvider>{children}</UploadProvider>
-          </TooltipProvider>
-        </AuthProvider>
-      </SessionProvider>
+      <AuthProvider>
+        <TooltipProvider>
+          <UploadProvider>{children}</UploadProvider>
+        </TooltipProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
